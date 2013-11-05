ec52181d910c80d691c8767ac7a83e53757593c3
Paul McLaughlin <paul@themclaughlins.me>
2013-11-05 22:48:32 +0000

    Adding in unit tests for comman

---
+++ NutritionCal.Test/Comman/BaseInformationFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NutritionCal.Common.Abstraction;
+using NutritionCal.Common.Implementation;
+using NUnit;
+using NUnit.Framework;
+
+namespace NutritionCal.Test.Comman
+{
+    [TestFixture]
+    public class BaseInformationFixture
+    {
+        [Test]
+        public void TestWeightSet()
+        {
+            IBaseInformation baseinformation = new BaseInformation();
+            baseinformation.Weight = 10;
+
+            Assert.AreEqual(10, baseinformation.Weight);
+        }
+    }
+}
---
+++ NutritionCal.Test/Comman/GoalFactoryFixture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NutritionCal.Common.Abstraction;
+using NutritionCal.Common.Implementation;
+using NutritionCal.Common;
+
+namespace NutritionCal.Test
+{
+    [TestClass]
+    public class GoalFactoryFixture
+    {
+        [TestMethod]
+        public void TestGoalTypeFactoryGain()
+        {
+            IGoalTypeFactory goalTypeFactory = new GoalTypeFactory();
+
+            IGoalType actualGoalType = goalTypeFactory.GetGoalType(GoalTypeEnum.GainWeight);
+
+            Assert.IsInstanceOfType(actualGoalType,typeof(GoalGainWeight));
+        }
+
+        [TestMethod]
+        public void TestGoalTypeFactoryLose()
+        {
+            IGoalTypeFactory goalTypeFactory = new GoalTypeFactory();
+
+            IGoalType actualGoalType = goalTypeFactory.GetGoalType(GoalTypeEnum.LoseWeight);
+
+            Assert.IsInstanceOfType(actualGoalType, typeof(GoalLoseWeight));
+        }
+
+        [TestMethod]
+        public void TestGoalTypeFactoryLoseObese()
+        {
+            IGoalTypeFactory goalTypeFactory = new GoalTypeFactory();
+
+            IGoalType actualGoalType = goalTypeFactory.GetGoalType(GoalTypeEnum.LoseWeightObese);
+
+            Assert.IsInstanceOfType(actualGoalType, typeof(GoalLoseWeightObese));
+        }
+
+        [TestMethod]
+        public void TestGoalTypeFactoryMaintain()
+        {
+            IGoalTypeFactory goalTypeFactory = new GoalTypeFactory();
+
+            IGoalType actualGoalType = goalTypeFactory.GetGoalType(GoalTypeEnum.MaintainWeight);
+
+            Assert.IsInstanceOfType(actualGoalType, typeof(GoalMaintainWeight));
+        }
+    }
+}
